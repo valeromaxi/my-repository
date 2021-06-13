54a845888a3f2227fada54a89a91799205a4d957
Maxi Valero <valeromaxi@gmail.com>
2021-06-13 18:10:30 -0300

    cambio 1

--- JS_Projects/arrays/busqueda_compara_arrays.js
+++ JS_Projects/arrays/busqueda_compara_arrays.js
@@ -204,6 +204,8 @@ const findMyKeys = key => key.indexOf('keys');
 const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
 
 console.log(findMyKeys(randomStuff));
+//cambio para prueba de git
+
 
 
 
