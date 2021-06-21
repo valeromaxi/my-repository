3b6a9bc71e9d7392a5bf987c0d11eb647d378aef
Maxi Valero <valeromaxi@gmail.com>
2021-06-21 11:46:38 -0300

    msj function

--- msj_generator/script.js
+++ msj_generator/script.js
@@ -1,6 +1,6 @@
 const arrMjs = [['Hola ', 'Buenas ', 'Que tal '], ['saamigos ', 'loquito loquito ', 'Bro '], ['Que Dios te Bendiga', 'Buena jornada', 'Forza!']];
-//console.log(arrMjs[0].length);
 
+//this function returns aleatory msj from arrMsj
 const msjGenerator = (newArr) => {
     let firstWord = newArr[0][Math.floor(Math.random() * newArr[0].length)];
     let secondWord = newArr[1][Math.floor(Math.random() * newArr[1].length)];
