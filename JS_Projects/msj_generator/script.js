dc0b970b12164aa886d86191d4d080eebe565cf4
Maxi Valero <valeromaxi@gmail.com>
2021-06-21 10:21:07 -0300

    newFunc nested arr

--- JS_Projects/msj_generator/script.js
+++ JS_Projects/msj_generator/script.js
@@ -1,8 +1,11 @@
-const arrMjs = [['Hola', 'Buenas', 'Que tal'], ['saamigos', 'loquito loquito', 'Bro'], ['Que Dios te Bendiga', 'Buena jornada', 'Forza!']];
-console.log(arrMjs[0][1]);
+const arrMjs = [['Hola ', 'Buenas ', 'Que tal '], ['saamigos ', 'loquito loquito ', 'Bro '], ['Que Dios te Bendiga', 'Buena jornada', 'Forza!']];
+//console.log(arrMjs[0][1]);
 
-/*const msjGenerator = (newArr) => {
-    return newArr[Math.floor(Math.random() * 3)];
+const msjGenerator = (newArr) => {
+    let firstWord = newArr[0][Math.floor(Math.random() * 3)];
+    let secondWord = newArr[1][Math.floor(Math.random() * 3)];
+    let thirdWord = newArr[2][Math.floor(Math.random() * 3)];
+    return firstWord + secondWord + thirdWord;
 }
 
-console.log(msjGenerator(arrMjs));*/
+console.log(msjGenerator(arrMjs));
